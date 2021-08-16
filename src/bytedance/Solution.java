d86af51af36a142f176d4022556e7909ad0aae35
terryhy520 <terryhy520@qq.com>
2021-08-16 19:38:21 +0800

    byte dance

---
+++ src/bytedance/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021. Terry All Rights Reserved.
+ * FileName: Solution.java
+ * @author: terryhy520@gmail.com
+ * @date: 8/16/21, 7:34 PM
+ * @version: 1.0
+ */
+
+package bytedance;
+
+/**
+ * @Author: Terry
+ * @Date: 8/16/2021 7:34 PM
+ */
+public class Solution {
+}
